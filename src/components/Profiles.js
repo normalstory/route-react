ed92f1550239e08218a390620ede7aa1d0bd65b7
thinknormal <nornalstory@gmail.com>
2020-11-22 14:21:40 +0900

    naviLink

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+// import { Route, Link } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import Profile from "./Profile";
+import "../style/nav.scss";
+import "../style/navLink.css";
 
 const Profiles = () => {
+  const activeStyle = {
+    //NavLink가 활성화 되었을 때
+    background: "black", //여기서는 ';' 아니고, ','
+    color: "white",
+  };
   return (
     <div>
       <h3>사용자 목록</h3>
       <ul>
         <li>
-          <Link to="/Profiles/gildong">홍길동</Link>
+          <NavLink
+            className="nonSelected"
+            //activeClassName="selectedScss" //scss로 활성화에 대한 별도 styling
+            activeClassName="selectedCSS" //css로 활성화에 대한 별도 styling
+            to="/Profiles/gildong"
+            active
+          >
+            홍길동
+          </NavLink>
         </li>
         <li>
-          <Link to="/Profiles/gilseo">홍길서</Link>
+          <NavLink
+            className="nonSelected"
+            activeStyle={activeStyle} //NavLink로 활성화에 대한 별도 styling
+            to="/Profiles/gilseo"
+          >
+            홍길서
+          </NavLink>
         </li>
       </ul>
 
