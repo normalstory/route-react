ad1a8b32f1d26904b8e8a46784f013ca84352df7
thinknormal <nornalstory@gmail.com>
2020-11-23 11:49:41 +0900

    api 호출 후 데이터 불러오기(then vs async)

--- src/components/AsyncAwait1.js
+++ src/components/AsyncAwait1.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const asyncAwait1 = () => {
+const AsyncAwait1 = () => {
+  const [data, setData] = useState(null);
+
+  // const onClick = () => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/todos/1")
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
-      <p>hello</p>
+      <button onClick={onClick}>불러오기</button>
+      <br />
+      {data && (
+        <textarea row={7} value={JSON.stringify(data, null, 2)} readOnly />
+      )}
     </div>
   );
 };
 
-export default asyncAwait1;
+export default AsyncAwait1;
